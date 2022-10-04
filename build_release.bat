39df74a7613b0a6f908bb789549ad7d2e68c70a3
Amos Vryhof <amos@vryhofresearch.com>
2022-10-04 15:08:26 -0400

    remove unneeded filetypes rename build-scripts

--- build_release.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-pip install --upgrade setuptools wheel twine
-python setup.py sdist bdist_wheel
-# Test PyPi
-#twine upload --repository-url https://test.pypi.org/legacy/ dist/*
-# Prod PyPi
-twine upload dist/*
